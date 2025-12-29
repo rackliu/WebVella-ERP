34228e1f1d04332c43b733f33c3e56ab937f2071
Bozhidar Zashev <bzashev@users.noreply.github.com>
2025-12-29 11:48:12 +0200

    project fixes

---
+++ WebVella.Erp.Plugins.Project/ProjectPlugin.20251229.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebVella.Erp.Api;
+using WebVella.Erp.Api.Models;
+using WebVella.Erp.Web.Models;
+
+namespace WebVella.Erp.Plugins.Project
+{
+	public partial class ProjectPlugin : ErpPlugin
+	{
+		private static void Patch20251229(EntityManager entMan, EntityRelationManager relMan, RecordManager recMan)
+		{
+
+            #region << ***Update data source*** Name: WvProjectTimeLogsForRecordId >>
+            {
+                var id = new Guid("e66b8374-82ea-4305-8456-085b3a1f1f2d");
+                var name = @"WvProjectTimeLogsForRecordId";
+                var description = @"Get all time logs for a record";
+                var eqlText = @"SELECT *,$user_1n_timelog.image,$user_1n_timelog.username
+FROM timelog
+WHERE l_related_records CONTAINS @recordId 
+ORDER BY @sortBy @sortOrder
+PAGE @page
+PAGESIZE @pageSize";
+                var sqlText = @"SELECT row_to_json( X ) FROM (
+SELECT 
+	 rec_timelog.""id"" AS ""id"",
+	 rec_timelog.""body"" AS ""body"",
+	 rec_timelog.""created_by"" AS ""created_by"",
+	 rec_timelog.""created_on"" AS ""created_on"",
+	 rec_timelog.""is_billable"" AS ""is_billable"",
+	 rec_timelog.""logged_on"" AS ""logged_on"",
+	 rec_timelog.""minutes"" AS ""minutes"",
+	 rec_timelog.""l_scope"" AS ""l_scope"",
+	 rec_timelog.""l_related_records"" AS ""l_related_records"",
+	 COUNT(*) OVER() AS ___total_count___,
+	------->: $user_1n_timelog
+	(SELECT  COALESCE( array_to_json( array_agg( row_to_json(d) )), '[]') FROM (
+	 SELECT 
+		 user_1n_timelog.""id"" AS ""id"",
+		 user_1n_timelog.""image"" AS ""image"",
+		 user_1n_timelog.""username"" AS ""username""
+	 FROM rec_user user_1n_timelog
+	 WHERE user_1n_timelog.id = rec_timelog.created_by ) d )::jsonb AS ""$user_1n_timelog""	
+	-------< $user_1n_timelog
+
+FROM rec_timelog
+WHERE  ( rec_timelog.""l_related_records""  ILIKE  CONCAT ( '%' , @recordId , '%' ) )
+ORDER BY rec_timelog.""created_on"" DESC
+LIMIT 1000
+OFFSET 0
+) X
+";
+                var parametersJson = @"[{""name"":""sortBy"",""type"":""text"",""value"":""created_on"",""ignore_parse_errors"":false},{""name"":""sortOrder"",""type"":""text"",""value"":""desc"",""ignore_parse_errors"":false},{""name"":""page"",""type"":""int"",""value"":""1"",""ignore_parse_errors"":false},{""name"":""pageSize"",""type"":""int"",""value"":""1000"",""ignore_parse_errors"":false},{""name"":""recordId"",""type"":""text"",""value"":""string.empty"",""ignore_parse_errors"":false}]";
+                var fieldsJson = @"[{""name"":""id"",""type"":16,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""body"",""type"":10,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""created_by"",""type"":16,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""created_on"",""type"":5,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""is_billable"",""type"":2,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""logged_on"",""type"":4,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""minutes"",""type"":12,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""l_scope"",""type"":18,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""l_related_records"",""type"":18,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""$user_1n_timelog"",""type"":20,""entity_name"":"""",""relation_name"":null,""children"":[{""name"":""id"",""type"":16,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""image"",""type"":9,""entity_name"":"""",""relation_name"":null,""children"":[]},{""name"":""username"",""type"":18,""entity_name"":"""",""relation_name"":null,""children"":[]}]}]";
+                var weight = 10;
+                var returnTotal = true;
+                var entityName = @"timelog";
+
+                new WebVella.Erp.Database.DbDataSourceRepository().Update(id, name, description, weight, eqlText, sqlText, parametersJson, fieldsJson, entityName, returnTotal);
+            }
+            #endregion
+
+
+
+
+        }
+    }
+}
